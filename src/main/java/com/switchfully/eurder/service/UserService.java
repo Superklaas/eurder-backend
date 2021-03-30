68e185673140396b0caf0148e781221c941d8a2a
Klaas Decock <klaasdc@icloud.com>
2021-03-30 15:14:15 +0200

    UserService created

---
+++ src/main/java/com/switchfully/eurder/service/UserService.java
@@ -0,0 +1,18 @@
+package com.switchfully.eurder.service;
+
+import com.switchfully.eurder.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+}
