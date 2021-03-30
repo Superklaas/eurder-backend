25d3dfcf568c375edef3f529cdb4019c0b58b7ca
Klaas Decock <klaasdc@icloud.com>
2021-03-30 10:51:22 +0200

    enabled swagger (pom & main app)

--- src/main/java/com/switchfully/eurder/EurderApplication.java
+++ src/main/java/com/switchfully/eurder/EurderApplication.java
@@ -2,8 +2,10 @@ package com.switchfully.eurder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EurderApplication {
 
     public static void main(String[] args) {
