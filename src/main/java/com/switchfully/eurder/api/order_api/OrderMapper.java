637376efe23820fbcd13aa80780929ac68fb8f8b
Klaas Decock <klaasdc@icloud.com>
2021-03-31 11:24:21 +0200

    OrderMapper toDto

--- src/main/java/com/switchfully/eurder/api/order_api/OrderMapper.java
+++ src/main/java/com/switchfully/eurder/api/order_api/OrderMapper.java
@@ -1,5 +1,6 @@
 package com.switchfully.eurder.api.order_api;
 
+import com.switchfully.eurder.domain.Order;
 import com.switchfully.eurder.domain.OrderUnit;
 import org.springframework.stereotype.Component;
 
@@ -17,4 +18,11 @@ public class OrderMapper {
         return orderUnitDtoList.stream().map(this::toOrderUnit).collect(Collectors.toList());
     }
 
+    public OrderDto toDto(Order order) {
+        return new OrderDto()
+                .setId(order.getId())
+                .setOrderUnits(order.getOrderUnits())
+                .setUser(order.getUser())
+                .setTotalPrice(order.getTotalPrice());
+    }
 }
