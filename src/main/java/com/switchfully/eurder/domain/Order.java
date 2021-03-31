3cb6622e9a98e10e81f3d5b1b920f757f9e618c4
Klaas Decock <klaasdc@icloud.com>
2021-03-31 09:41:40 +0200

    added field id to Order

--- src/main/java/com/switchfully/eurder/domain/Order.java
+++ src/main/java/com/switchfully/eurder/domain/Order.java
@@ -1,16 +1,23 @@
 package com.switchfully.eurder.domain;
 
 import java.util.List;
+import java.util.UUID;
 
 public class Order {
 
+    private String id;
     private List<OrderUnit> orderUnits;
     private User user;
     private double totalPrice;
 
     public Order(List<OrderUnit> orderUnits, User user) {
+        this.id= UUID.randomUUID().toString();
         this.orderUnits = orderUnits;
         this.user = user;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public List<OrderUnit> getOrderUnits() {
