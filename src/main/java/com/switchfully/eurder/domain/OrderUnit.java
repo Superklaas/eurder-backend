db7380ea121475ba6212f5844c11b2a929c437d5
Klaas Decock <klaasdc@icloud.com>
2021-03-31 09:35:49 +0200

    OrderUnit created

--- src/main/java/com/switchfully/eurder/domain/OrderUnit.java
+++ src/main/java/com/switchfully/eurder/domain/OrderUnit.java
@@ -1,4 +1,39 @@
 package com.switchfully.eurder.domain;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class OrderUnit {
+
+    private final String id;
+    private final Item item;
+    private final int amount;
+    private LocalDate shippingDate;
+
+    public OrderUnit(Item item, int amount) {
+        this.id= UUID.randomUUID().toString();
+        this.item = item;
+        this.amount = amount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public LocalDate getShippingDate() {
+        return shippingDate;
+    }
+
+    public OrderUnit setShippingDate(LocalDate shippingDate) {
+        this.shippingDate = shippingDate;
+        return this;
+    }
 }
