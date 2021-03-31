7b50527f5021f03d8043c0719be3605cc3a758f9
Klaas Decock <klaasdc@icloud.com>
2021-03-31 13:47:19 +0200

    removed field id from OrderUnit

--- src/main/java/com/switchfully/eurder/domain/OrderUnit.java
+++ src/main/java/com/switchfully/eurder/domain/OrderUnit.java
@@ -5,19 +5,13 @@ import java.util.UUID;
 
 public class OrderUnit {
 
-    private final String id;
     private final Item item;
     private final int amount;
     private LocalDate shippingDate;
 
     public OrderUnit(Item item, int amount) {
-        this.id= UUID.randomUUID().toString();
         this.item = item;
         this.amount = amount;
-    }
-
-    public String getId() {
-        return id;
     }
 
     public Item getItem() {
