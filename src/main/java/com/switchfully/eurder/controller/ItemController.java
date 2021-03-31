68b79f822a6151b74302bce3b781c40a9ad9f315
Klaas Decock <klaasdc@icloud.com>
2021-03-31 18:55:38 +0200

    authorization createItem

--- src/main/java/com/switchfully/eurder/controller/ItemController.java
+++ src/main/java/com/switchfully/eurder/controller/ItemController.java
@@ -4,6 +4,7 @@ import com.switchfully.eurder.dto.CreateItemDto;
 import com.switchfully.eurder.dto.ItemDto;
 import com.switchfully.eurder.mapper.ItemMapper;
 import com.switchfully.eurder.service.ItemService;
+import com.switchfully.eurder.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,16 +17,20 @@ public class ItemController {
 
     private final ItemService itemService;
     private final ItemMapper itemMapper;
+    private final UserService userService;
 
     @Autowired
-    public ItemController(ItemService itemService, ItemMapper itemMapper) {
+    public ItemController(ItemService itemService, ItemMapper itemMapper, UserService userService) {
         this.itemService = itemService;
         this.itemMapper = itemMapper;
+        this.userService = userService;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ItemDto createItem(@RequestBody CreateItemDto input) {
+    public ItemDto createItem(@RequestBody CreateItemDto input,
+                              @RequestHeader String userId) {
+        userService.assertAdmin(userId);
         return itemMapper.toDto(itemService.createItem(itemMapper.toItem(input)));
     }
 
